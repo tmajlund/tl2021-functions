6d0ddf34fd9dcdb6d11aefa684be885c018c3fd7
Therese Berg <70585763+thereseberg@users.noreply.github.com>
2021-09-27 15:46:27 +0200

    added class table data to function MessageReader

--- MessageReader.cs
+++ MessageReader.cs
@@ -7,11 +7,22 @@ namespace AzNan.MessageReader
     public static class MessageReader
     {
         [Function("MessageReader")]
-        public static void Run([QueueTrigger("votings", Connection = "AzureWebJobsStorage")] string myQueueItem,
+        [TableOutput("Votings", Connection = "AzureWebJobsStorage")]
+        public static TableData Run([QueueTrigger("votings", Connection = "AzureWebJobsStorage")] NewVote vote,
             FunctionContext context)
         {
             var logger = context.GetLogger("MessageReader");
             logger.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
     }
+
+    public class TableData
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public string UserInfo { get; set; }
+        public string Vote { get; set; }
+    }
 }
+
+//NewVote is a class not created yet. Need to understand the communication between api in swa-client and function-app
